50b07c1408ce58c8c3594508a2c4e1c7c33ac51b
vuppalli <vu8hh@virginia.edu>
2020-06-04 11:33:07 -0400

    deleted data ingestion folder to submit PR

--- data-science-onramp/DataIngestion/setup.sh
+++
@@ -1,6 +0,0 @@
-# Submit a PySpark job via the Cloud Dataproc Jobs API
-gcloud dataproc jobs submit pyspark \
-    --cluster ${CLUSTER_NAME} \
-    --jars gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar \
-    --driver-log-levels root=FATAL \
-    setup.py -- ${BUCKET_NAME}
